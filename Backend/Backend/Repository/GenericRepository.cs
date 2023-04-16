310d95282c0e6d0175ab7f3fde3ef6f11a8d8328
mamin98 <mohamedamin1421@gmail.com>
2023-04-16 02:03:10 +0200

    imp specific evaluater

--- Backend/Backend/Repository/GenericRepository.cs
+++ Backend/Backend/Repository/GenericRepository.cs
@@ -1,5 +1,6 @@
 ﻿using Backend.Data;
 using Backend.Entities;
+using Backend.Specification;
 using Microsoft.EntityFrameworkCore;
 
 namespace Backend.Repository
@@ -18,9 +19,25 @@ namespace Backend.Repository
             return await _context.Set<T>().FindAsync(id);
         }
 
+        
         public async Task<IReadOnlyList<T>> ListAllAsync()
         {
             return await _context.Set<T>().ToListAsync();
+        }
+
+        public async Task<T> GetEntityWithSpec(ISpecification<T> spec)
+        {
+            return await ApplySpecification(spec).FirstOrDefaultAsync();
+        }
+
+        public async Task<IReadOnlyList<T>> ListAsync(ISpecification<T> spec)
+        {
+            return await ApplySpecification(spec).ToListAsync();
+        }
+
+        private IQueryable<T> ApplySpecification(ISpecification<T> spec)
+        {
+            return SpecificationEvaluater<T>.GetQuery(_context.Set<T>().AsQueryable(), spec);
         }
     }
 }
