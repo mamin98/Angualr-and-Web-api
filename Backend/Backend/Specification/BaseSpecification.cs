b0f77a0663af3fd6341f063157b2172f84415084
mamin98 <mohamedamin1421@gmail.com>
2023-04-19 00:48:24 +0200

    add el sorting specification

--- Backend/Backend/Specification/BaseSpecification.cs
+++ Backend/Backend/Specification/BaseSpecification.cs
@@ -13,9 +13,23 @@ namespace Backend.Specification
 
         public List<Expression<Func<T, object>>> Includes { get; } = new List<Expression<Func<T, object>>>();
 
+        public Expression<Func<T, object>> OrderBy { get; private set; }
+
+        public Expression<Func<T, object>> OrderByDescending { get; private set; }
+
         protected void AddIncludes(Expression<Func<T, object>> includes)
         {
             Includes.Add(includes);
+        }
+
+        protected void AddOrderBy(Expression<Func<T, object>> orderByExp)
+        {
+            OrderBy = orderByExp;
+        }
+
+        protected void AddOrderByDescending(Expression<Func<T, object>> orderByDescExp)
+        {
+            OrderByDescending = orderByDescExp;
         }
     }
 }
--- Backend/Backend/Specification/ISpecification.cs
+++ Backend/Backend/Specification/ISpecification.cs
@@ -7,5 +7,8 @@ namespace Backend.Specification
     {
         Expression<Func<T, bool>> Criteria { get; }
         List<Expression<Func<T, object>>> Includes { get; }
+        Expression<Func<T, object>> OrderBy { get; }
+        Expression<Func<T, object>> OrderByDescending { get; }
+
     }
 }
